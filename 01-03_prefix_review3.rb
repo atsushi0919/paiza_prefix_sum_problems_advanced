1f7a2c2ca812d485d31948aa0b92b56acf67f5f4
atsushi0919 <hatake0919@yahoo.co.jp>
2022-05-27 14:56:48 +0900

    セクション1 解答追加

--- 01-03_prefix_review3.rb
+++ 01-03_prefix_review3.rb
@@ -1,3 +1,6 @@
+# 【累積和の練習問題】復習問題 その 3 (paizaランク C 相当)
+# https://paiza.jp/works/mondai/prefix_sum_problems_advanced/prefix_sum_problems_advanced__prefix_review_step3
+
 INPUT1 = <<~"EOS"
   13 3 8 a
   paizalearning
@@ -27,65 +30,3 @@ def solve(input_str)
 end
 
 puts solve(STDIN.read)
-
-=begin
-【累積和の練習問題】復習問題 その 3 (paizaランク C 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
-
-シェア用URL:
-https://paiza.jp/works/mondai/prefix_sum_problems_advanced/prefix_sum_problems_advanced__prefix_review_step3
-問題文のURLをコピーする
- チャレンジする言語
-
-得意な言語を選択してください
-コードを書いて解いてみる
- チケット使用済
-問題
- 下記の問題をプログラミングしてみよう！
-1 行目に整数 N, X, Y と文字 C が与えられます。
-
-2 行目に小文字のアルファベットで構成された長さ N の文字列 str が与えられます。
-
-文字列 str の X 文字目から Y 文字目までの文字 C の個数を累積和を用いて求め、一行で出力してください。
-
-入力される値
-1 行目に整数 N, X, Y と文字 C が与えられます。
-
-2 行目に小文字のアルファベットで構成された長さ N の文字列 str が与えられます。
-
-
-N X Y C
-str
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-文字列 str の X 文字目から Y 文字目までの文字 C の個数を累積和を用いて求め、一行で出力してください。
-
-末尾に改行を入れ、余計な文字、空行を含んではいけません。
-
-条件
-すべてのテストケースにおいて、以下の条件をみたします。
-
-・ 1 ≦ N ≦ 1,000
-
-・ 1 ≦ X ≦ Y ≦ N
-
-・ C は小文字のアルファベット一文字
-
-・ str は小文字のアルファベット (a~z) からなる長さ N の文字列
-
-入力例1
-13 3 8 a
-paizalearning
-
-出力例1
-2
-
-入力例2
-30 10 20 p
-papapaizapapaizapaizapapapaiza
-
-出力例2
-3
-=end
--- 01-07_prefix_practice4.rb
+++ 01-07_prefix_practice4.rb
@@ -1,3 +1,6 @@
+# 【累積和の練習問題】練習問題 その 4 (paizaランク B 相当)
+# https://paiza.jp/works/mondai/prefix_sum_problems_advanced/prefix_sum_problems_advanced__prefix_practice_step4
+
 INPUT1 = <<~"EOS"
   1
   1 100
@@ -37,66 +40,3 @@ def solve(input_str)
 end
 
 puts solve(STDIN.read)
-
-=begin
-【累積和の練習問題】練習問題 その 4 (paizaランク B 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
-
-シェア用URL:
-https://paiza.jp/works/mondai/prefix_sum_problems_advanced/prefix_sum_problems_advanced__prefix_practice_step4
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-1 行目に整数 Q が与えられます。
-
-2 行目以降に Q 行に渡って整数 L_i, R_i (1 ≦ i ≦ Q) が与えられます。
-
-各 L_i, R_i について、L_i ≦ x ≦ R_i かつ 3 と 5 の両方で割り切れるような整数 x の個数を累積和を用いて求め、
-改行区切りで出力してください。
-
-▼　下記解答欄にコードを記入してみよう
-
-入力される値
-・ 1 行目に整数 Q が与えられます。
-
-・ 2 行目以降に Q 行に渡って整数 L_i, R_i (1 ≦ i ≦ Q) が与えられます。
-
-
-Q
-L_1 R_1
-L_2 R_2
-...
-L_Q R_Q
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-各 L_i, R_i について、L_i ≦ x ≦ R_i かつ 3 と 5 の両方で割り切れるような整数 x の個数を累積和を用いて求め、
-改行区切りで出力してください。
-
-末尾に改行を入れ、余計な文字、空行を含んではいけません。
-
-条件
-すべてのテストケースにおいて、以下の条件をみたします。
-
-・ 1 ≦ Q ≦ 100
-
-・ 1 ≦ L_i ≦ R_i ≦ 1,000 (1 ≦ i ≦ Q)
-
-入力例1
-1
-1 100
-
-出力例1
-6
-
-入力例2
-3
-1 15
-15 100
-138 813
-
-出力例2
-1
-6
-45
-=end
